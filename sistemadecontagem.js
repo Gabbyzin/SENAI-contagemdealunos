f136e13966893cb9fda601535edb22630b2d8a32
Gabriela <gabidpm27@gmail.com>
2022-08-18 21:50:17 -0300

    Contagem de Alunos

---
+++ sistemadecontagem.js
@@ -0,0 +1,10 @@
+let listadeacademia = ["Feliphe","Vitória","Luiz","Anna","Vivian"]
+
+for (let index = 0; index < ListadeAlunos.length; index++) {
+    console.log (index)
+    if (index ==0){
+        console.log ("zero localizado:" + ListadeAlunos [index])
+    } else if (index % 2) {console.log ("impar:" + ListadeAlunos [index])}
+    else [console.log ("par:" + ListadeAlunos [index])]
+    console.log [ListadeAlunos]
+}
